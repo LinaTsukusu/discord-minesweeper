c18eeb1a2fa57ffdf412f6a9563d10c36066aebf
neverclear86 <neverclear86sab@gmail.com>
2019-02-02 19:00:24 +0900

    できた

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import {Client} from 'discord.js'
-import * as program from 'commander'
+import * as yargs from 'yargs/yargs'
 import {generateGame} from './minesweeper'
 
 
@@ -8,17 +8,36 @@ client.login(process.env.TOKEN)
 
 client.on('message', (message) => {
   const channel = message.channel
-  const command = message.content.split(' ')
-  if (message.author.bot || channel.id !== process.env.CHANNEL_ID || command[0] !== '/minesweeper') {
+  const parser = yargs()
+    .option('width', {
+      alias: 'w',
+      description: 'Set width',
+      type: 'number',
+      default: 10,
+    })
+    .option('height', {
+      alias: 'h',
+      description: 'Set height',
+      type: 'number',
+      default: 10,
+    })
+    .option('bombs', {
+      alias: 'b',
+      description: 'Set bombs amount',
+      type: 'number',
+      default: 10,
+    })
+    .version(false)
+  const args = parser.parse(message.content.split(' '))
+
+  if (message.author.bot || channel.id !== process.env.CHANNEL_ID || args._[0] !== '/minesweeper' && args._[0] !== '/ms') {
     return
   }
-  program.version('1.0.0')
-    .alias('ms')
-    .description('マインスイーパーを生成します')
-    .option("-w, --width <width>", "Set width", parseInt,10)
-    .option("-h, --height <height>", "Set height", parseInt,10)
-    .option("-b, --bombs <bombs>", "Set bombs amount", parseInt,10)
-    .parse(command)
-
-  channel.send(generateGame(program.width, program.height, program.bombs))
+  const game = generateGame(args.width, args.height, args.bombs)
+  if (game.length <= 1900) {
+    channel.send(game)
+  } else {
+    // 1900文字超えると壊れる
+    channel.send("でかすぎるっぽい")
+  }
 })
--- tests/minesweeper.spec.ts
+++ tests/minesweeper.spec.ts
@@ -1,9 +1,26 @@
 import {generateGame} from "@/minesweeper"
 
+function testGame(width: number, height: number, bombs: number) {
+  const game = generateGame(width, height, bombs)
+  expect(game.match(/\|\|:\w+?:\|\|/g)).toHaveLength(width * height)
+  expect(game.match(/\n/g)).toHaveLength(height)
+  expect(game.match(/:bomb:/g)).toHaveLength(bombs)
+}
+
 describe("MineSweeper", () => {
-  it("generateGame", () => {
-    const game = generateGame(10, 10, 10)
-    expect(game.match(/\|\|:\w+?:\|\|/g)).toHaveLength(10 * 10)
-    expect(game.match(/:bomb:/g)).toHaveLength(10)
+  it("default", () => {
+    testGame(10, 10, 10)
+  })
+
+  it("20 10 10", () => {
+    testGame(20, 10, 10)
+  })
+
+  it("10 10 30", () => {
+    testGame(10, 10, 30)
+  })
+
+  it("10 20 10", () => {
+    testGame(10, 20, 10)
   })
 })
